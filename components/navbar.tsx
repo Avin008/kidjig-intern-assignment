d77d8ac0a5fa93748f9b131b9f8bc31d9f6790b5
avinash <avinashnanda008@gmail.com>
2024-07-19 16:23:05 +0530

    style: nav bg color

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 const Navbar = () => {
-  return <nav className="h-20 border-b fixed left-0 right-0 top-0"></nav>;
+  return <nav className="h-20 fixed left-0 right-0 top-0 bg-[#000000]"></nav>;
 };
 
 export default Navbar;
